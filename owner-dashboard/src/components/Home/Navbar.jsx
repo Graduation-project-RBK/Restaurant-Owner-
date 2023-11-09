2c4790f14b0cc6d13af8d2da6d03194d99738247
Jawhar <138862392+JawharZinelabidine@users.noreply.github.com>
2023-11-09 19:02:33 +0100

    added notification badge

--- owner-dashboard/src/components/Home/Navbar.jsx
+++ owner-dashboard/src/components/Home/Navbar.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { NavLink } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import axios from "axios";
+import { setNotificationBadge } from "../../features/notificationSlice";
 
 import './Nav.css'
 
 function NavBar() {
+    const [notificationNumber, setNotificationNumber] = useState(1)
+
+    const dispatch = useDispatch();
+
+    const { ownerId } = useSelector(state => state.restaurant);
+    const { notificationBadge } = useSelector(state => state.notification);
+    const checkNotification = async () => {
+        try {
+            const { data } = await axios.get(`http://localhost:3000/api/owners/notification/${ownerId}`)
+            dispatch(setNotificationBadge(data))
+
+            console.log(notificationBadge)
+        } catch (error) {
+            console.log(error)
+        }
+
+        if (notificationBadge) {
+
+            try {
+                const response = await axios.get(`http://localhost:3000/api/restaurants/${ownerId}`)
+
+                const { data } = await axios.get(`http://localhost:3000/api/reservations/pending/${response.data.id}`)
+                setNotificationNumber(data.length)
+            } catch (error) {
+                console.log(error)
+
+            }
+        }
+    }
+
+    const removeNotificationBadge = async () => {
+        try {
+            const { data } = await axios.put(`http://localhost:3000/api/owners/notification/${ownerId}`)
+            dispatch(setNotificationBadge(data))
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
+
+    useEffect(() => {
+        checkNotification()
+    }, [])
 
     return (
         <nav className="navbar">
@@ -17,7 +65,8 @@ function NavBar() {
                             <NavLink to="/home">Dashboard</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/reservation-list">Pending Reservation Table</NavLink>
+                            <NavLink to="/reservation-list" onClick={removeNotificationBadge}>Pending Reservation Table<span id={notificationBadge ? "circle" : "noCircle"}
+                            > {notificationNumber}</span></NavLink>
                         </li>
                         <li>
                             <NavLink to="/reservation-history">Reservation History</NavLink>
