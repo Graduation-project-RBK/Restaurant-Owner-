0b14c146d3f1c69962bdcf37533c79d4a9740b04
Jawhar <138862392+JawharZinelabidine@users.noreply.github.com>
2023-11-13 17:49:16 +0100

    login now checks if owner is banned

--- owner-dashboard/src/components/Home/Reviews.jsx
+++ owner-dashboard/src/components/Home/Reviews.jsx
@@ -3,25 +3,23 @@ import './Reviews.css';
 import NavBar from "./Navbar";
 
 
-const Reviews =()=>{
-    return(
+const Reviews = () => {
+    return (
         <div>
-            <NavBar/>
-        <div className="container">
-    <div className="mgb-40 padb-30 auto-invert line-b-4 align-center">
-        <h1 className="font-cond-b fg-text-d lts-md fs-300 fs-300-xs no-mg" contenteditable="false">Read Customer Reviews</h1>
-    </div>
-    <ul className="hash-list cols-3 cols-1-xs pad-30-all align-center text-sm">
-        <li>
-          <img src="https://bootdey.com/img/Content/avatar/avatar1.png" className="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]"/>
-          <p className="fs-110 font-cond-l" contenteditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
-          <h5 className="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Martha Stewart</h5>
-          <small className="font-cond case-u lts-sm fs-80 fg-text-l" contenteditable="false">Business Woman - New York</small>
-        </li>
-    
-      </ul>
-</div>
-</div>
+            <NavBar />
+            <div className="container">
+                <div className="mgb-40 padb-30 auto-invert line-b-4 align-center">
+                    <h1 className="font-cond-b fg-text-d lts-md fs-300 fs-300-xs no-mg" contenteditable="false">Read Customer Reviews</h1>
+                </div>
+                <ul className="hash-list pad-30-all align-center text-sm">
+                    <li>
+                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" className="wpx-100 img-round mgb-20 image" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
+                        <h5 className="font-cond mgb-5 fg-text-d fs-130 " id="title" contenteditable="false">Martha Stewart</h5>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     )
 }
 export default Reviews;
