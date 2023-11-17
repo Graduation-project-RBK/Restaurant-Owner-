ae7d53e4bf2b124b42f1db0890aaf870b5c3df1a
JawharZinelabidine <jawharzedd@gmail.com>
2023-11-17 02:29:54 +0100

    set up messages component connection with api

--- owner-dashboard/src/components/Chat/Conversations.jsx
+++ owner-dashboard/src/components/Chat/Conversations.jsx
@@ -1,24 +1,41 @@
 import axios from "../../../services/axios-interceptor";
 import { useEffect, useState } from "react";
 import "./Conversations.css";
+import porfileImage from '../../assets/images/man.png'
 
 
 
+const Conversations = ({ customerId }) => {
 
-const Conversations = () => {
+    const [name, setName] = useState('')
 
+    const findCustomerName = async () => {
+        try {
+            const { data } = await axios.get(`http://localhost:3000/api/owners/customers/${customerId}`)
+            setName(data.fullname)
 
+        } catch (error) {
+            console.log(error)
+            if (error.response.status === 403 || error.response.status === 401) {
+                localStorage.clear()
+                navigate('/')
+            }
+        }
+    }
 
-
+    useEffect(() => {
+        findCustomerName()
+        console.log()
+    }, [])
 
     return (
         <div className="conversation">
             <img
                 className="conversationImg"
-                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                src={porfileImage}
                 alt=""
             />
-            <span className="conversationName">John Doe</span>
+            <span className="conversationName">{name}</span>
 
         </div>
     )
